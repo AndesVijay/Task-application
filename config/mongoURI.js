2016122ade0d6cc65b3c760827e72e5fe68629a4
AndesVijay <91484512+AndesVijay@users.noreply.github.com>
2021-11-19 18:08:04 +0530

    Update mongoURI.js

--- config/mongoURI.js
+++ config/mongoURI.js
@@ -1,1 +1,1 @@
-module.exports="mongodb+srv://AndesVijay:x6cRVM6z-hZaeii@cluster1.jrgfb.mongodb.net/TODOList?retryWrites=true&w=majority"; 
+module.exports="mongodb+srv://AndesVijay:9udagX3ln3AsIRzU@cluster1.jrgfb.mongodb.net/TODOList?retryWrites=true&w=majority";
